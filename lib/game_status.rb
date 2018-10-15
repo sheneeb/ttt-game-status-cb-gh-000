5400ac6ff33955e4019ddd85c3947575840505cd
Sheneeb Sadhik <sadhik.sheneeb@gmx.com>
2018-10-15 17:37:43 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -71,7 +71,7 @@ won?(board).each do |win_combination|
 
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
     return 'X'
-  elsif 
+  elsif
      position_1 == "O" && position_2 == "O" && position_3 == "O"
       return 'X'
 end
